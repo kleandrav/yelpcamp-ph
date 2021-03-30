a852bae124b176c1587d8b61b5ca77516a681ae6
Karla Vargas <kleandrav@gmail.com>
2021-03-30 16:36:58 +0800

    add log-in page

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 const {remove} = require('../models/user');
@@ -8,7 +9,33 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    res.send(req.body);
+    try
+    {
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+
+        // register() - convenience method to register a new user instance with a given password. Checks if username is unique. [passport-local-mongoose]
+        const registeredUser = await User.register(user, password);
+        // console.log(registeredUser);
+
+        req.flash('success', `Welcome to YelpCamp, ${username}!`);
+        res.redirect('/campgrounds');
+    }
+    catch (err)
+    {
+        req.flash('error', err.message);
+        res.redirect('register');
+    }
 })
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+router.post('/login', 
+    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        res.redirect('/campgrounds');
+});
 
 module.exports = router;
