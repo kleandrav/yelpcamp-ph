851982998efb474a1b2fbeaf153e0570474a4c92
Karla Vargas <kleandrav@gmail.com>
2021-04-07 19:27:37 +0800

    prevent image overload

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,8 @@
 // deleting a campground 
-
 const deleteBtn = document.getElementById('delete-btn');
 const loader = document.querySelector('.loader-wrapper');
 
-if( deleteBtn ) {
+if (deleteBtn) {
     deleteBtn.onclick = (event) => {
         let willDelete = confirm('Do you want to delete this campground?');
         
@@ -12,7 +11,25 @@ if( deleteBtn ) {
     };
 }
 
-// async-btn
+// adding images
+const addImageBtn = document.querySelector('.adds-images');
+if (addImageBtn) {
+    const imageInput = document.getElementById('new-camp-images');
+    const fileLabel = document.querySelector('.custom-file-label');
+    addImageBtn.onclick = (event) => {
+        if (imageInput.files.length > 3) {
+            event.preventDefault();
+            // console.log( imageInput.files.length + " files entered." );
+            imageInput.classList.add('invalid');
+            fileLabel.classList.add('invalid-text');
+            fileLabel.innerText = "Maximum of 3 photos only.";
+        } else {
+            loader.style.display = "flex";
+        }
+    }
+}
+
+// for async buttons such as log-in & register
 const asyncBtn = document.querySelector('.async-btn');
 if (asyncBtn) {
     asyncBtn.onclick = () => {
