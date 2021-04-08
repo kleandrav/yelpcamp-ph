1d3b012070914c382b1b1087fc0affb683d66b68
Karla Vargas <kleandrav@gmail.com>
2021-04-09 04:31:12 +0800

    fix env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // import modules
 const express = require('express');
